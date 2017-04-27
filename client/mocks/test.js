d5dbefaae2f6a487b371194c4820fb1f9a6a1d1c
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-04-27 16:23:56 +0300

    Modified 4 items, Deleted 4 items, New 2 items

--- client/mocks/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.test = window.mocks.test || {};
-window.mocks.test.testAction = function ($scope, params, app) {
-};
-window.mocks.test.array_action1 = function ($scope, params, app) {
-};
-window.mocks.test.array_action2 = function ($scope, params, app) {
-};
