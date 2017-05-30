13b11428d8bda69a67c58bba09d336aafea25812
test <qqq@q>
2017-05-30 11:56:34 +0300

    [Studio] Add screen "Categories-Cool"

--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.categoriescool', {
+        views: {
+            app: {
+                controller: 'app_categoriescool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.categoriescool');
+                }
+            }
+        }
     });
 });
